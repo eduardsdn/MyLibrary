b449c2bcbbe8e1d1cf8bd7f0798172f6bff1286f
eduardsdn <eduard.sdn@gmail.com>
2022-11-04 12:55:34 +0100

    Form event listener now createsBook object and appends it to the books array

--- library.js
+++ library.js
@@ -4,3 +4,34 @@ function Book(title, author, year, isRead) {
     this.year = year
     this.isRead = isRead
 }
+
+books = []
+
+let form = document.querySelector(".newBookForm")
+
+
+
+
+
+
+form.addEventListener('submit', function(event) {
+    let bookName = document.getElementById("name").value
+    let bookAuthor = document.getElementById("author").value
+    let bookYear = document.getElementById("year").value
+    let isReadCheckboxes = document.querySelectorAll('input[type="checkbox"]')
+    let isReadValue = null
+    
+    for(let i = 0; i < Array.from(isReadCheckboxes).length; i++){
+        if (Array.from(isReadCheckboxes)[i].checked) {
+            isReadValue = Array.from(isReadCheckboxes)[i].value
+            break
+        }
+    }
+    event.preventDefault()
+    newBook = new Book(bookName, bookAuthor, bookYear, isReadValue)
+    books.push(newBook)
+   
+    console.log(newBook)
+})
+
+console.log(books)
